1c96e053232e976d0449a2d0e62db340c03f2d2b
wujunwei <wjw3323@live.com>
2022-05-27 11:20:45 +0800

    test
    Signed-off-by: wujw39640 <wujw39640@hundsun.com>

--- src/pkg/token/claims/v2/claims.go
+++ src/pkg/token/claims/v2/claims.go
@@ -1,6 +1,7 @@
 package v2
 
 import (
+	"crypto/subtle"
 	"fmt"
 
 	"github.com/docker/distribution/registry/auth/token"
@@ -23,7 +24,7 @@ func (c *Claims) Valid() error {
 	if err := c.StandardClaims.Valid(); err != nil {
 		return err
 	}
-	if !c.VerifyIssuer(Issuer, true) {
+	if subtle.ConstantTimeCompare([]byte(c.Issuer), []byte(Issuer)) == 0 {
 		return fmt.Errorf("invalid token issuer: %s", c.Issuer)
 	}
 	return nil
